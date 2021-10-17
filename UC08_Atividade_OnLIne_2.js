e1a6e9f7c166b37ee1f8ef041f1871d90e4bf62c
RonaldKSilva <ronaldksilva@gmail.com>
2021-10-16 22:34:56 -0300

    Atualizar atividades UC 08

--- UC08_Atividade_OnLIne_2.js
+++ UC08_Atividade_OnLIne_2.js
@@ -9,60 +9,112 @@ let continuar = 1;
 
 while (continuar > 0) { // Executar a entrada de dados até a opção do usuário -> continuar: SIM (1) ou NÃO (0).
 
-    let nomeDoEvento = "SENAI - Full Stack - Turma 02";
-    let dataDoEvento = "30/10/2021";
-    let lotacaoDoEvento = 100;
+    let tituloDoEvento = "SENAI - Full Stack - Turma 02";
+    let dataDoEvento = "30/11/2021";
+    let lotacaoDePalestrantes = 10;
+    let lotacaoDeParticipantes = 100;
     let idadeMinimaNoEvento = 18;
+    let listaDePalestrantesCadastrados = [];
+    let listaDeParticipantesCadastrados = [];
+    let listaDeIdadeDosParticipantesCadastrados = [];
 
-    if (dataDoEvento >= "13/10/2021") { // Teste de Coerência da Data Informada para o Evento
+    if (dataDoEvento >= "18/10/2021") { // Teste de Coerência da Data Informada para o Evento
 
         let listaDeInteressados = ["Helena", "Mário", "José", "Maria", "João", "Paulo", "Mateus", "Madalena", "Joana", "Ester"];
-        let listaDeIdadeDoInteressado = ["21", "13", "55", "25", "18", "35", "29", "44", "30", "27"];
+        let listaDeIdadeDoInteressado = ["21", "17", "55", "25", "18", "35", "29", "44", "30", "27"];
+        let listaDePalestrantes = ["BigOne", "MasterTech", "AllonMind"];
+        let quantidadeDePalestrantes = 0;
         let quantidadeDeParticipantes = 0;
         let date = Date();
 
-        console.log("\n\n##############################################################");
-        console.log(date);
-        console.log("\nNome do Evento =", nomeDoEvento);
-        console.log("Data do Evento =", dataDoEvento);
-        console.log("______________________________________________________________\n");
+        // Inicia o cadastro do evento
+
+        console.log("\n\n\n[[[[[ SISTEMA PARA CADASTRO DE EVENTOS ]]]]]");
+        console.log("\nInformar o Título do Evento:\n");
+        console.log(tituloDoEvento);
+        console.log("\nInformar a Data do Evento:\n");
+        console.log(dataDoEvento);
+
+        // Inicia o cadastro de Palestrantes
+
+        for (let localizador = 0; localizador < listaDePalestrantes.length; localizador++) { // O loop foi usado devido a entrada de dados ser pela lista no código
+
+            if (quantidadeDePalestrantes < lotacaoDePalestrantes) { // Teste da Lotação de Palestrantes no Evento
+
+                console.log("\nInformar nome do palestrante:\n")
+                console.log("Nome =", listaDePalestrantes[localizador]);
+                listaDePalestrantesCadastrados.push(listaDePalestrantes[localizador]);
+                quantidadeDePalestrantes++;
+
+            } else {
+
+                console.log("\nEvento com Lotação de Palestrantes Esgotada =", quantidadeDePalestrantes, "palestrantes.\n");
+
+            } // End IF Palestrantes
+
+        } // End For Palestrantes
+
+        // Inicia o cadastro de Participantes
 
         for (let localizador = 0; localizador < listaDeInteressados.length; localizador++) { // O loop foi usado devido a entrada de dados ser pela lista no código
 
-            if (quantidadeDeParticipantes < lotacaoDoEvento) { // Teste da Lotação no Evento
+            if (quantidadeDeParticipantes < lotacaoDeParticipantes) { // Teste da Lotação de Participantes no Evento
+
+                console.log("\nInformar Nome e Idade do Interesssado:\n");
+                console.log("Nome =", listaDeInteressados[localizador]);
+                console.log("Idade =", listaDeIdadeDoInteressado[localizador]);
 
                 if (listaDeIdadeDoInteressado[localizador] >= idadeMinimaNoEvento) { // Teste da Idade Mínima do Interessado
 
+                    listaDeParticipantesCadastrados.push(listaDeInteressados[localizador]);
+                    listaDeIdadeDosParticipantesCadastrados.push(listaDeIdadeDoInteressado[localizador]);
                     quantidadeDeParticipantes++;
-                    console.log(quantidadeDeParticipantes, "Nome =", listaDeInteressados[localizador], "--> Idade =", listaDeIdadeDoInteressado[localizador], "anos");
 
                 } else {
 
-                    console.log("  Nome =", listaDeInteressados[localizador], "--> Não é possível cadastrar interessado Menor de Idade (", listaDeIdadeDoInteressado[localizador], "anos)");
+                    console.log(">>>>> Não é possível cadastrar interessado Menor de Idade !\n");
 
                 } // END IF Teste de Idade
-
-                ;
 
                 console.log("Continuar Cadastro de Interessados no Evento ?"); // Opção hipotética para o Usuário escolher
                 // Atribuímos que nesta condição o usuário deve prosseguir -> continuar = 1
 
-                if (continuar > 0) {
-                    console.log("SIM\n");
+                if (continuar > 0) {  // Opção hipotética de continuar 
+                    if ((localizador + 1) == listaDeInteressados.length) {
+                        console.log("Não"); // Quando alcançar o final da lista de entrada
+                    } else {
+                        console.log("Sim"); // Enquando não alcançar o final da lista de entrada 
+                    }
                 } else {
-                    console.log("Não\n");
+                    console.log("Não");
                     break;
                 } // End IF Opção de Continuar após cadastro de interessado
 
             } else {
 
-                console.log("\nEvento com Lotação Esgotada =", quantidadeDeParticipantes, "participantes.\n");
+                console.log("\nEvento com Lotação de Participantes Esgotada =", quantidadeDeParticipantes, "participantes.\n");
 
             } // End IF Teste de Quantidade de Participantes
 
         } // END FOR Entrada de Dados
 
-        console.log("\nTotal de Participantes Cadastrados =", quantidadeDeParticipantes);
+        console.log("\n\n##############################################################");
+        console.log(date);
+        console.log("\nNome do Evento =", tituloDoEvento);
+        console.log("Data do Evento =", dataDoEvento);
+        console.log("______________________________________________________________\n");
+        console.log("*******         RELATÓRIO DO CADASTRO NO EVENTO        *******");
+
+        console.log("\nTotal de Palestrantes Cadastrados =", quantidadeDePalestrantes, "\n");
+        for (contador = 0; contador < quantidadeDePalestrantes; contador++) {
+            console.log((contador + 1), "Nome =", listaDePalestrantesCadastrados[contador]);
+        }
+
+        console.log("\nTotal de Participantes Cadastrados =", quantidadeDeParticipantes, "\n");
+        for (contador = 0; contador < quantidadeDeParticipantes; contador++) {
+            console.log((contador + 1), "Nome =", listaDeParticipantesCadastrados[contador], "--> Idade =", listaDeIdadeDosParticipantesCadastrados[contador], "anos");
+        }
+
         console.log("\nFim do Cadastro de Eventos e Participantes.");
         console.log("\n<<<<< ------------------------------- >>>>>\n\n");
 
@@ -74,7 +126,7 @@ while (continuar > 0) { // Executar a entrada de dados até a opção do usuári
         console.log("Continuar Cadastro ?"); // Opção hipotética para o Usuário escolher
         // Atribuímos que nesta condição o usuário deve prosseguir -> continuar = 1;
 
-        if (continuar > 0) {
+        if (continuar > 0) { // // Opção hipotética de continuar 
             console.log("SIM\n");
         } else {
             console.log("Não\n");
